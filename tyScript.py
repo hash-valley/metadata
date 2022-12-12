72282d2f0034dbd4b3016923b046c3e5476ebb3b
Nate Mueller <inathan.m@gmail.com>
2022-12-11 22:27:05 -0800

    script

--- tyScript.py
+++ tyScript.py
@@ -13,31 +13,31 @@ def format_json(num, mints, v1minter):
         {
             "name": f"Hash Valley Winery Early Supporter #{num}",
             "external_url": "https://hashvalley.xyz",
-            "image": "ipfs://QmSUcGhzAQWo1EF42Yqj8XLX4LfHShMsrLwrtKE1z9KSV1",
+            "image": "ipfs://bafybeiapbv7y2ivmcspegc3lswwy4cmdavndedwopuci3mogcoyo4cdaja",
             "description": message,
             "attributes": attributes
         },
         indent=4)
 
-    # Writing to sample.json
+    # Writing to json
     with open(f"ty/{num}.json", "w") as outfile:
         outfile.write(ty_json)
 
 
 import os
-  
-# checking if the directory demo_folder 
+
+# checking if the directory demo_folder
 # exist or not.
 if not os.path.exists("ty"):
-      
-    # if the demo_folder directory is not present 
+
+    # if the demo_folder directory is not present
     # then create it.
     os.makedirs("ty")
 
 v1_minters = [17, 3, 15, 7, 6, 1, 9, 19, 1, 5, 4, 4, 8]
 other = 6
 for i in range(len(v1_minters) + other):
-    if i < 13:
+    if i < len(v1_minters):
         format_json(i, v1_minters[i], True)
     else:
         format_json(i, 3, False)
